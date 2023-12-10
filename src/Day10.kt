9dd21aa717ad017be8c66c922b6cfbce6513c2f7
Alexey Illarionov <alexey@0xdc.ru>
2023-12-10 13:51:51 +0300

    Day10 dirty 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,21 +1,26 @@
+import Border.BOTTOM
+import Border.BOTTOM_RIGHT
+import Border.NO
+import Border.RIGHT
+
 fun main() {
     val testInput = readInput("Day10_test").parse()
     val testInput2 = readInput("Day10_test2").parse()
     val input = readInput("Day10").parse()
 
-    part1(testInput).also {
-        println("Part 1, test input: $it")
-        check(it == 8)
-    }
-
-    part1(input).also {
-        println("Part 1, real input: $it")
-        check(it == 6842)
-    }
+//    part1(testInput).also {
+//        println("Part 1, test input: $it")
+//        check(it == 8)
+//    }
+//
+//    part1(input).also {
+//        println("Part 1, real input: $it")
+//        check(it == 6842)
+//    }
 
     part2(testInput2).also {
         println("Part 2, test input: $it")
-        check(it == 10)
+        check(it == 8)
     }
 
     part2(input).also {
@@ -28,7 +33,7 @@ fun main() {
 private fun List<String>.parse(): Field = Field(this.map { it.toList() })
 
 fun Field(
-    tiles: List<List<Char>>
+    tiles: List<List<Char>>,
 ): Field {
     val startPoint = run {
         for (row in tiles.indices) {
@@ -89,21 +94,25 @@ private fun Field.eastIfCanTraverse(pos: Pos): Pos? {
 }
 
 private fun part1(input: Field): Int {
-    return listOf('|', '-', 'L', 'J', '7', 'F').maxOf {
+    return getLongestPathWithPath(input).first
+}
+
+private fun getLongestPathWithPath(input: Field): Triple<Int, Set<Pos>, Char> {
+    return listOf('|', '-', 'L', 'J', '7', 'F').map {
         val newField = input.withTileAtStartPoint(it)
-        newField.getLongestPath()
-    }
+        val path = newField.getLongestPath()
+        Triple(path.first, path.second, it)
+    }.maxBy { it.first }
 }
 
 private class DeqItem(
     val left: Pos,
     val right: Pos,
-    val leftFrom: Pos,
-    val rightFrom: Pos,
-    val distance: Int
+    val path: Set<Pos>,
+    val distance: Int,
 )
 
-private fun Field.getLongestPath(): Int {
+private fun Field.getLongestPath(): Pair<Int, Set<Pos>> {
     val startPos = this.startPoint
     val firstDirs = listOfNotNull(
         northIfCanTraverse(startPos),
@@ -111,7 +120,7 @@ private fun Field.getLongestPath(): Int {
         westIfCanTraverse(startPos),
         eastIfCanTraverse(startPos),
     )
-    if (firstDirs.size < 2) return -1
+    if (firstDirs.size < 2) return -1 to emptySet()
 
     val deq: ArrayDeque<DeqItem> = ArrayDeque()
 
@@ -120,16 +129,18 @@ private fun Field.getLongestPath(): Int {
             val leftPos = firstDirs[leftIndex]
             val rightPos = firstDirs[rightIndex]
             deq.add(
-                DeqItem(leftPos, rightPos, startPos, startPos, 1)
+                DeqItem(leftPos, rightPos, setOf(startPos, leftPos, rightPos), 1),
             )
         }
     }
 
     var max = -1
+    var maxPath: Set<Pos> = emptySet()
     while (deq.isNotEmpty()) {
         val path: DeqItem = deq.removeLast()
         if (path.left == path.right) {
             max = maxOf(path.distance, max)
+            maxPath = path.path + path.left
             continue
         }
 
@@ -138,8 +149,8 @@ private fun Field.getLongestPath(): Int {
             southIfCanTraverse(path.left),
             westIfCanTraverse(path.left),
             eastIfCanTraverse(path.left),
-        ). filter {
-            it != path.leftFrom && it != path.rightFrom
+        ).filter {
+            it !in path.path
         }
         val newRight = listOfNotNull(
             northIfCanTraverse(path.right),
@@ -147,23 +158,185 @@ private fun Field.getLongestPath(): Int {
             westIfCanTraverse(path.right),
             eastIfCanTraverse(path.right),
         ).filter {
-            it != path.leftFrom && it != path.rightFrom
+            it !in path.path
         }
         if (newLeft.isEmpty() || newRight.isEmpty()) {
             continue
         }
         newLeft.forEach { l ->
             newRight.forEach { r ->
-                val item = DeqItem(l, r, path.left, path.right, path.distance + 1)
+                val item = DeqItem(
+                    l,
+                    r,
+                    path.path + l + r,
+                    path.distance + 1,
+                )
                 deq.add(item)
             }
         }
     }
 
-    return max
-
+    return max to maxPath
 }
 
+private enum class Border { NO, BOTTOM, RIGHT, BOTTOM_RIGHT }
+
+private data class PosWithBorder(
+    val row: Int,
+    val column: Int,
+    val border: Border,
+)
+
 private fun part2(input: Field): Int {
-    return 0
+    val longestPathPart1 = getLongestPathWithPath(input)
+    val path: Set<Pos> = longestPathPart1.second
+    val field = input.withTileAtStartPoint(longestPathPart1.third)
+
+    val pathExtended = path.flatMap {
+        when (field[it]) {
+            '|' -> listOf(
+                PosWithBorder(it.row, it.col, NO),
+                PosWithBorder(it.row, it.col, BOTTOM),
+            )
+            '-' -> listOf(
+                PosWithBorder(it.row, it.col, NO),
+                PosWithBorder(it.row, it.col, RIGHT),
+            )
+            'L' -> listOf(
+                PosWithBorder(it.row, it.col, NO),
+                PosWithBorder(it.row, it.col, RIGHT),
+            )
+            'J' -> listOf(
+                PosWithBorder(it.row, it.col, NO),
+            )
+            '7' -> listOf(
+                PosWithBorder(it.row, it.col, NO),
+                PosWithBorder(it.row, it.col, BOTTOM),
+            )
+            'F' -> listOf(
+                PosWithBorder(it.row, it.col, NO),
+                PosWithBorder(it.row, it.col, RIGHT),
+                PosWithBorder(it.row, it.col, BOTTOM),
+            )
+            else -> listOf(
+                PosWithBorder(it.row, it.col, NO),
+            )
+        }
+    }.toSet()
+
+    val start = PosWithBorder(-1, -1, Border.NO)
+    val outside: MutableSet<PosWithBorder> = mutableSetOf(start)
+    val deq: ArrayDeque<PosWithBorder> = ArrayDeque()
+
+    deq.add(start)
+    outside.add(start)
+
+    while (deq.isNotEmpty()) {
+        val item = deq.removeLast()
+        if (item in pathExtended) {
+            continue
+        }
+
+        outside.add(item)
+        field.adjacentOf(item).forEach {
+            if (it !in outside) {
+                deq.add(it)
+            }
+        }
+    }
+    field.printFieldWithPath(path, outside)
+
+    return field.tiles.flatMapIndexed { row, columns ->
+        columns.filterIndexed { column, value ->
+            Pos(row, column) !in path && PosWithBorder(row, column, NO) !in outside
+        }
+    }.count()
+}
+
+private fun Field.adjacentOf(pos: PosWithBorder): List<PosWithBorder> = buildList {
+    val fieldRows = this@adjacentOf.tiles.size
+    val fieldCols = this@adjacentOf.tiles[0].size
+    when (pos.border) {
+        NO -> {
+            // left
+            if (pos.column > -1) add(
+                PosWithBorder(row = pos.row, column = pos.column - 1, border = RIGHT),
+            )
+            //right
+            add(PosWithBorder(row = pos.row, column = pos.column, border = RIGHT))
+
+            // top
+            if (pos.row > -1) add(
+                PosWithBorder(row = pos.row - 1, column = pos.column, border = BOTTOM),
+            )
+
+            // bottom
+            add(PosWithBorder(row = pos.row, column = pos.column, border = BOTTOM))
+        }
+
+        BOTTOM -> {
+            // left
+            if (pos.column > -1) add(
+                PosWithBorder(row = pos.row, column = pos.column - 1, border = BOTTOM_RIGHT),
+            )
+            // Right
+            add(PosWithBorder(row = pos.row, column = pos.column, border = BOTTOM_RIGHT))
+            // Top
+            add(PosWithBorder(row = pos.row, column = pos.column, border = NO))
+            // Bottom
+            if (pos.row <= fieldRows) {
+                add(PosWithBorder(row = pos.row + 1, column = pos.column, border = NO))
+            }
+        }
+        RIGHT -> {
+            // Top
+            if (pos.row > -1) add(
+                PosWithBorder(row = pos.row - 1, column = pos.column, border = BOTTOM_RIGHT),
+            )
+            // Bottom
+            add(PosWithBorder(row = pos.row, column = pos.column, border = BOTTOM_RIGHT))
+            // left
+            add(PosWithBorder(row = pos.row, column = pos.column, border = NO))
+            // right
+            if (pos.column <= fieldCols) {
+                add(PosWithBorder(row = pos.row, column = pos.column + 1, border = NO))
+            }
+        }
+
+        BOTTOM_RIGHT -> {
+            // Top
+            add(PosWithBorder(row = pos.row, column = pos.column, border = RIGHT))
+            // Bottom
+            if (pos.row <= fieldRows) {
+                add(PosWithBorder(row = pos.row + 1, column = pos.column, border = RIGHT))
+            }
+            // Left
+            add(PosWithBorder(row = pos.row, column = pos.column, border = BOTTOM))
+            // Right
+            if (pos.column <= fieldCols) {
+                add(PosWithBorder(row = pos.row, column = pos.column + 1, border = BOTTOM))
+            }
+        }
+    }
+}
+
+private fun Field.printFieldWithPath(path: Set<Pos>, outside: Set<PosWithBorder>) {
+    this.tiles.forEachIndexed { row, rowChars ->
+        val rowString = rowChars.mapIndexed { column, char ->
+            if (Pos(row, column) in path) {
+                when (char) {
+                    'L' -> '└'
+                    'J' -> '┘'
+                    '7' -> '┐'
+                    'F' -> '┌'
+                    else -> char
+                }
+            } else if (PosWithBorder(row, column, NO) in outside){
+                'O'
+            } else {
+                'I'
+            }
+        }.joinToString("")
+        println(rowString)
+    }
 }
